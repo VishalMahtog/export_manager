04e085fe31a9ff21e6a1d073fd1a1a094ad31a9b
Vishal Mahto <vishal.mahto@softices.in>
2024-12-26 16:12:06 +0530

    Dynamic get table and column name and export csv

---
+++ app/controllers/export_manager/export_manager_controller.rb
@@ -0,0 +1,36 @@
+module ExportManager
+  class ExportManagerController < ApplicationController
+    def export
+      @model_name = ActiveRecord::Base.connection.tables.map do |t|
+        formatted_name = t&.gsub("_", " ")&.titleize&.gsub(" ", "")
+        [ formatted_name, t ]
+      end
+    end
+
+    def send_coloum_name
+      columns = ActiveRecord::Base.connection.columns(params["table"]).map(&:name)
+      render json: columns
+    end
+
+    def dowload_csv
+      request = params
+      model_name = request["table"].singularize.camelize
+      dataes = "#{model_name}".constantize.all
+
+      request.delete("action")
+      request.delete("controller")
+      request.delete("table")
+      request.delete("select-all")
+      column = request.keys
+
+      csv_data = CSV.generate(headers: true) do |csv|
+        csv << column.map(&:capitalize)
+        dataes.each do |data|
+          csv << column.map { |col| data.send(col) }
+        end
+      end
+
+      send_data csv_data, filename: "export_#{Date.today}.csv", type: "text/csv"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 ExportManager::Engine.routes.draw do
+  get "export", to: "export_manager#export", as: :export_export_manager_index
+  get "send_coloum_name", to: "export_manager#send_coloum_name"
+  get "dowload_csv", to: "export_manager#dowload_csv"
 end
