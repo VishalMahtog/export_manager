7bfa840613a8c71bafd084312d4bb538fff38f54
Vishal Mahto <vishal.mahto@softices.in>
2024-12-27 12:08:11 +0530

    code refactor

--- app/controllers/export_manager/export_manager_controller.rb
+++ app/controllers/export_manager/export_manager_controller.rb
@@ -9,7 +9,7 @@ module ExportManager
         next if remove_table.include?(t.downcase)
 
         formatted_name = t.gsub("_", " ").titleize.gsub(" ", "")
-        [formatted_name, t]
+        [ formatted_name, t ]
       end.compact
 
       if remove_table.present?
@@ -43,19 +43,19 @@ module ExportManager
 
     def set_global_variable
       case params[:type]
-      when 'csv'
+      when "csv"
         @file_name = defined?(CSV_FILE_NAME) ? CSV_FILE_NAME : "export_#{Date.today}.csv"
-      when 'excel'
+      when "excel"
         @file_name = defined?(EXCEL_FILE_NAME) ? EXCEL_FILE_NAME : "export_#{Date.today}.xlsx"
       end
     end
 
     def export_data(records, columns, file_name)
       case params[:type]
-      when 'csv'
+      when "csv"
         csv_data = ExportManager.generate_csv(records, columns)
         send_csv(csv_data, file_name)
-      when 'excel'
+      when "excel"
         excel_data = ExportManager.generate_excel(records, columns)
         send_excel(excel_data, file_name)
       end
@@ -69,7 +69,7 @@ module ExportManager
       send_data excel_data,
                 filename: file_name,
                 type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                disposition: 'attachment'
+                disposition: "attachment"
     end
   end
 end
